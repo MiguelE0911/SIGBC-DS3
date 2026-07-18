e177edb5b9e5dde4a107e8403b1dd8a286691c96
Miguel <miguel.aparicio3@utp.ac.pa>
2026-07-17 20:25:50 -0500

    test: Rama Admin

---
+++ src/main/java/com/bmw/cine/administrador/controller/Main.java
@@ -0,0 +1,7 @@
+package com.bmw.cine.administrador.controller;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Rama Admin");
+    }
+}
---
+++ src/main/java/com/bmw/cine/personal/controller/Main.java
@@ -0,0 +1,7 @@
+package com.bmw.cine.personal.controller;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Rama Personal");
+    }
+}
